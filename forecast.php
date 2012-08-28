2f53afea404ba854eb6dd39323a0f444b35f9e24
lyonna <t@lyonna.me>
2012-08-28 22:03:42 +0800

    再次重写，大幅简化代码

--- forecast.php
+++ forecast.php
@@ -1,219 +1,129 @@
 <?php
-  function getWeatherData($cityid) {
+  date_default_timezone_set('Asia/Shanghai');
+  $city = '';
+  $body = '<h3>城市代码不合法！</h3>';
+
+  if (isset($_GET['cid']) && strlen($_GET['cid'])==9 && ctype_digit($_GET['cid'])) {
+    $forecast = getforecast($_GET['cid']);
+    if ($forecast) {
+      $city = $forecast['city'];
+      $body = printbody($forecast);
+    }
+  }
+
+  function getforecast($cityid) {
     if (!function_exists('curl_init')) {
-      do {
-        $weatherdata = file_get_contents('http://m.weather.com.cn/data/'.$cityid.'.html');
-      } while ($weatherdata == '');
+      $forecast = file_get_contents('http://m.weather.com.cn/data/'.$cityid.'.html');
     }
     else {
       $ch = curl_init();
       curl_setopt($ch, CURLOPT_URL, 'http://m.weather.com.cn/data/'.$cityid.'.html');
       curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
       curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-      do {
-        $weatherdata = curl_exec($ch);
-      } while ($weatherdata == '');
+      $forecast = curl_exec($ch);
+      curl_close($ch);
     }
-    curl_close($ch);
-    $weatherdata = json_decode($weatherdata, TRUE);
-    return $weatherdata['weatherinfo'];
-  }
-
-  function getDateNum($datedata) {
-    return sscanf($datedata, "%d%*[^0-9]%d%*[^0-9]%d");
-  }
-
-  function dayToNum($weekday) {
-    switch($weekday) {
-      case '星期一':
-        $daynum = 0;
-        break;
-      case '星期二':
-        $daynum = 1;
-        break;
-      case '星期三':
-        $daynum = 2;
-        break;
-      case '星期四':
-        $daynum = 3;
-        break;
-      case '星期五':
-        $daynum = 4;
-        break;
-      case '星期六':
-        $daynum = 5;
-        break;
-      case '星期日':
-        $daynum = 6;
-        break;
+    $forecast = json_decode($forecast, TRUE);
+    if ($forecast) {
+      return $forecast['weatherinfo'];
     }
-    return $daynum;
+    return false;
   }
 
-  function getDay($daynum) {
-    $days = array('星期一','星期二','星期三','星期四','星期五','星期六','星期日');
-    return $days[$daynum%7];
+  function getdatetime($datestr, $hour) {
+    return strtotime(preg_replace(array('/年/', '/月/', '/日/'), array('-', '-', ''), $datestr)." {$hour}:00:00");
   }
 
-  function getNextDate($date) {
-    $year = $date[0];
-    $month = $date[1];
-    $day = $date[2];
-
-    switch($month) {
-      case 1:
-      case 3:
-      case 5:
-      case 7:
-      case 8:
-      case 10:
-      case 12:
-        if ($day == 31) {
-          if ($month == 12) {$year++; $month = 1;}
-          else {$month++;}
-          $day = 1;
-        }
-        else {
-          $day++;
-        }
-        break;
-      case 4:
-      case 6:
-      case 9:
-      case 11:
-        if ($day == 31) {$month++; $day = 1;}
-        else {$day++;}
-        break;
-      case 2:
-        if (($year%4 == 0 && ($year%100) != 0) || $year%400 == 0) {
-          if ($day == 29) {$month++; $day = 1;}
-          else {$day++;}
-        }
-        else {
-          if ($day == 28) {$month++; $day = 1;}
-          else {$day++;}
-        }
-        break;
-    }
-    return array($year, $month, $day);
+  function getday($datetime) {
+    $days = array('星期日','星期一','星期二','星期三','星期四','星期五','星期六');
+    return $days[date('w', $datetime)];
   }
 
-  function getImagesUrlFromData($weatherdata) {
-    if ($weatherdata['fchh'] != '18') {
+  function getimageurl($forecast) {
+    if ($forecast['fchh'] != '18') {
       $images[] = null;
       $remainder = 1;
     }
     else {
       $remainder = 0;
     }
+
+    $preurl = 'http://www.weather.com.cn/m/i/icon_weather/42x30/';
     for ($i = 1; $i < 13; $i++) {
-      if (strlen($weatherdata["img$i"]) == 1) {$weatherdata["img$i"] = "0".$weatherdata["img$i"];}
-      if ($weatherdata["img$i"] == "99") {$weatherdata["img$i"] = $weatherdata['img'.($i-1)];}
-      if ($i%2 == $remainder) {$images[] = 'http://www.weather.com.cn/m/i/icon_weather/42x30/d'.$weatherdata["img{$i}"].'.gif';}
-      else {$images[] = 'http://www.weather.com.cn/m/i/icon_weather/42x30/n'.$weatherdata["img{$i}"].'.gif';}
+      $addzero = strlen($forecast["img$i"]) == 1 ? '0' : '';
+      $forecast["img$i"] = $addzero.$forecast["img$i"];
+
+      if ($forecast["img$i"] == '99') {$forecast["img$i"] = $forecast['img'.($i-1)];}
+      $images[] = $i%2 == $remainder ? "{$preurl}d{$forecast["img{$i}"]}.gif" : "{$preurl}n{$forecast["img{$i}"]}.gif";
     }
     return $images;
   }
 
-  function fixWeatherData($weatherdata) {
+  function fixforecast($forecast) {
     for ($i = 1; $i < 6; $i++) {
-      $fixedweatherdata['weather'.$i][0] = explode('转', $weatherdata['weather'.$i]);
-      $fixedweatherdata['weather'.$i][1] = explode('转', $weatherdata['weather'.($i+1)]);
-      $fixedweatherdata['temp'.$i][0] = explode('~', $weatherdata['temp'.$i]);
-      $fixedweatherdata['temp'.$i][1] = explode('~', $weatherdata['temp'.($i+1)]);
+      $fix['weather'.$i][0] = explode('转', $forecast['weather'.$i]);
+      $fix['weather'.$i][1] = explode('转', $forecast['weather'.($i+1)]);
+      $fix['temp'.$i][0] = explode('~', $forecast['temp'.$i]);
+      $fix['temp'.$i][1] = explode('~', $forecast['temp'.($i+1)]);
 
-      if (preg_match('/(转.+风)/', $weatherdata['wind'.$i])) {
-        preg_match('/转([微东南西北风]+)/', $weatherdata['wind'.$i], $fixedweatherdata['wind'.$i][0]);
+      preg_match('/(转.+风)/', $forecast['wind'.$i]) ? preg_match('/转([微东南西北风]+)/', $forecast['wind'.$i], $fix['wind'.$i][0]) : preg_match('/([微东南西北风]+)/', $forecast['wind'.$i], $fix['wind'.$i][0]);
+      $fix['wind'.$i][0] = $fix['wind'.$i][0][1];
+      preg_match('/^([微东南西北风]+)/', $forecast['wind'.($i+1)], $fix['wind'.$i][1]);
+      $fix['wind'.$i][1] = $fix['wind'.$i][1][1];
+      preg_match('/([0-9-大小于级]+)$/', $forecast['fl'.$i], $fix['fl'.$i][0]);
+      $fix['fl'.$i][0] = $fix['fl'.$i][0][1];
+      preg_match('/^([0-9-大小于级]+)/', $forecast['fl'.($i+1)], $fix['fl'.$i][1]);
+      $fix['fl'.$i][1] = $fix['fl'.$i][1][1];
+
+      (strlen($fix['wind'.$i][0]) % 3) != 0 && $fix['wind'.$i][0] = substr_replace($fix['wind'.$i][0], '', strlen($fix['wind'.$i][0])-strlen($fix['wind'.$i][0]) % 3);
+      (strlen($fix['wind'.$i][1]) % 3) != 0 && $fix['wind'.$i][1] = substr_replace($fix['wind'.$i][1], '', strlen($fix['wind'.$i][1])-strlen($fix['wind'.$i][1]) % 3);
+
+      if ($fix['fl'.$i][0] == $fix['fl'.$i][1]) {
+        $fix['wind'.$i] = $fix['wind'.$i][0] == $fix['wind'.$i][1] ? $fix['wind'.$i][0].$fix['fl'.$i][0] : $fix['wind'.$i][0].'转'.$fix['wind'.$i][1].$fix['fl'.$i][0];
       }
       else {
-        preg_match('/([微东南西北风]+)/', $weatherdata['wind'.$i], $fixedweatherdata['wind'.$i][0]);
-      }
-      $fixedweatherdata['wind'.$i][0] = $fixedweatherdata['wind'.$i][0][1];
-      preg_match('/^([微东南西北风]+)/', $weatherdata['wind'.($i+1)], $fixedweatherdata['wind'.$i][1]);
-      $fixedweatherdata['wind'.$i][1] = $fixedweatherdata['wind'.$i][1][1];
-      preg_match('/([0-9-大小于级]+)$/', $weatherdata['fl'.$i], $fixedweatherdata['fl'.$i][0]);
-      $fixedweatherdata['fl'.$i][0] = $fixedweatherdata['fl'.$i][0][1];
-      preg_match('/^([0-9-大小于级]+)/', $weatherdata['fl'.($i+1)], $fixedweatherdata['fl'.$i][1]);
-      $fixedweatherdata['fl'.$i][1] = $fixedweatherdata['fl'.$i][1][1];
-
-      (strlen($fixedweatherdata['wind'.$i][0]) % 3) != 0 && $fixedweatherdata['wind'.$i][0] = substr_replace($fixedweatherdata['wind'.$i][0], '', strlen($fixedweatherdata['wind'.$i][0])-strlen($fixedweatherdata['wind'.$i][0]) % 3);
-      (strlen($fixedweatherdata['wind'.$i][1]) % 3) != 0 && $fixedweatherdata['wind'.$i][1] = substr_replace($fixedweatherdata['wind'.$i][1], '', strlen($fixedweatherdata['wind'.$i][1])-strlen($fixedweatherdata['wind'.$i][1]) % 3);
-
-      if ($fixedweatherdata['fl'.$i][0] == $fixedweatherdata['fl'.$i][1]) {
-        if ($fixedweatherdata['wind'.$i][0] == $fixedweatherdata['wind'.$i][1]) {
-          $fixedweatherdata['wind'.$i] = $fixedweatherdata['wind'.$i][0].$fixedweatherdata['fl'.$i][0];
-        }
-        else {
-          $fixedweatherdata['wind'.$i] = $fixedweatherdata['wind'.$i][0].'转'.$fixedweatherdata['wind'.$i][1].$fixedweatherdata['fl'.$i][0];
-        }
-      }
-      else {
-        if ($fixedweatherdata['wind'.$i][0] == $fixedweatherdata['wind'.$i][1]) {
-          $fixedweatherdata['wind'.$i] = $fixedweatherdata['wind'.$i][0].$fixedweatherdata['fl'.$i][0].'转'.$fixedweatherdata['fl'.$i][1];
-        }
-        else {
-          $fixedweatherdata['wind'.$i] = $fixedweatherdata['wind'.$i][0].$fixedweatherdata['fl'.$i][0].'转'.$fixedweatherdata['wind'.$i][1].$fixedweatherdata['fl'.$i][1];
-        }
+        $fix['wind'.$i] = $fix['wind'.$i][0] == $fix['wind'.$i][1] ? $fix['wind'.$i][0].$fix['fl'.$i][0].'转'.$fix['fl'.$i][1] : $fix['wind'.$i][0].$fix['fl'.$i][0].'转'.$fix['wind'.$i][1].$fix['fl'.$i][1];
       }
 
-      empty($fixedweatherdata['weather'.$i][0][1]) && $fixedweatherdata['weather'.$i][0][1] = $fixedweatherdata['weather'.$i][0][0];
+      empty($fix['weather'.$i][0][1]) && $fix['weather'.$i][0][1] = $fix['weather'.$i][0][0];
 
-      $fixedweatherdata['weather'.$i] = ($fixedweatherdata['weather'.$i][0][1] == $fixedweatherdata['weather'.$i][1][0]) ? $fixedweatherdata['weather'.$i][0][1] : $fixedweatherdata['weather'.$i][0][1].'转'.$fixedweatherdata['weather'.$i][1][0];
-      $fixedweatherdata['temp'.$i] = $fixedweatherdata['temp'.$i][1][0].'~'.$fixedweatherdata['temp'.$i][0][1];
+      $fix['weather'.$i] = $fix['weather'.$i][0][1] == $fix['weather'.$i][1][0] ? $fix['weather'.$i][0][1] : $fix['weather'.$i][0][1].'转'.$fix['weather'.$i][1][0];
+      $fix['temp'.$i] = $fix['temp'.$i][1][0].'~'.$fix['temp'.$i][0][1];
     }
-    return $fixedweatherdata;
+    return $fix;
   }
 
-  function printHead($city) { ?>
+  function printbody($forecast) {
+    function printdate($datetime) {
+      return date('n月j日', $datetime).' '.getday($datetime);
+    }
+    $date = getdatetime($forecast['date_y'], $forecast['fchh']);
+    $images = getimageurl($forecast);
+    $length = 7;
+    $body = "<ul>\n<li>".date('Y-m-d H时', $date)."发布</li><br />\n";
+
+    if ($forecast['fchh'] == '18') {
+      $date += 86400;
+      $forecast = fixforecast($forecast);
+      $length = 6;
+    }
+
+    for ($i = 1; $i < $length; $i++) {
+      $body .= '<li>'.printdate($date)."<br />\n";
+      $body .= '<img src="'.$images[($i*2-1)].'" /><img src="'.$images[($i*2)]."\" /><br />\n";
+      $body .= "{$forecast["weather$i"]} {$forecast["temp$i"]}<br />\n{$forecast["wind$i"]}</li>\n<br />";
+      $date += 86400;
+    }
+    return $body;
+  }
+?>
 <!doctype html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 <title><?php echo $city; ?>天气预报</title>
 </head>
-<?php
-  }
-
-  function printReleaseTime($date, $time) { ?>
 <body>
-<ul>
-<li><?php echo "{$date[0]}-{$date[1]}-{$date[2]} {$time}";?>时发布</li><br />
-<?php
-  }
-
-  function printWeather($weatherdata, $date, $daynum, $images, $length) {
-    for ($i = 1; $i < $length; $i++) {
-      echo "<li>{$date[1]}月{$date[2]}日 ".getDay($daynum+$i-1).'<br />';
-      echo '<img src="'.$images[($i*2-1)].'" /><img src="'.$images[($i*2)].'" /><br />';
-      echo "{$weatherdata["weather$i"]} {$weatherdata["temp$i"]}<br />{$weatherdata["wind$i"]}</li>\n<br />";
-      $date = getNextDate($date);
-    }
-  }
-
-  if (!isset($_GET["cid"]) || strlen($_GET["cid"])!=9 || !ctype_digit($_GET["cid"])) {
-    printHead('');
-    echo "<body>\n<h3>城市代码不合法！</h3>";
-  }
-  else {
-    $weatherdata = getWeatherData($_GET["cid"]);
-    $date = getDateNum($weatherdata['date_y']);
-    $daynum = dayToNum($weatherdata['week']);
-    $images = getImagesUrlFromData($weatherdata);
-    $length = 7;
-
-    printHead($weatherdata['city']);
-    printReleaseTime($date, $weatherdata['fchh']);
-
-    if ($weatherdata['fchh'] == '18') {
-      $date = getNextDate($date);
-      $daynum++;
-      $weatherdata = fixWeatherData($weatherdata);
-      $length = 6;
-    }
-
-    printWeather($weatherdata, $date, $daynum, $images, $length);
-  }
-?>
-
+<?php echo $body; ?>
 </body>
 </html>
